8bcdf44ec9aaef8855bd308c9e6641be4c16e825
camilaibanezbarrera <bindianonima@gmail.com>
2025-03-16 15:33:34 -0500

    Actualizacion

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigo = [];
+function agregarAmigo(){
+	let inputAmigo = document.getElement("amigo");
+	let nombreAmigo = inputAmigo.value;
+	if(!nombreAmigo){
+	   alert("Debes ingresar un nombre")
+           return;
+        }
+	amigo.push(nombreAmigo);
+	inputAmigo.value = "";
+	inputAmigo.focus();
+	renderizarAmigos()
+};
+
+function renderizarAmigo(){
+	let listaAmigos = document.getElemenById("listaAmigos");
+	listaAmigos.innerHTML = "";
+	
+	for(let i = 0; i < amigo.length; i++){
+	    let item = document.createElement("li");
+	    item.textContent = amigo[i];
+	    listaAmigos.appenchild(item);
+	}
+}
+function sortearAmigo(){
+	if(amigo.length === 0) {
+	alert("No hay amigos para sortear");
+	return;
+	}
+	let amigoSorteado = amigo[Math.floor(Math.random() * amigo.length)];
+	let resultado = document.getElementById("resultado");
+	resultado.innerHTML =  `El amigo sorteado es: ${amigoSorteado}`;
+    let limpiarlista = document.getElementById("listaAmigos");
+    limpiarlista.innerHTML="";
+}
